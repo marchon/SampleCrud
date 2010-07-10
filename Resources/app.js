b87389151540f1e37d065a4524d4bad7c124a5cc
Mark Stocks <mark@stocksfoundation.com>
2010-07-09 20:46:19 -0500

    updated app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,18 +1,19 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-//Here's the first window...
-var first = Ti.UI.createWindow({
+//Create Window
+var win = Ti.UI.createWindow({
   backgroundColor:"#999",
   title:"SampleCRUD",
   url: 'crud.js'
 });
 
-first.barColor = '#336699';
+//Set Bar Color
+win.barColor = '#336699';
 
 //Here's the nav group that will hold them both...
 var navGroup = Ti.UI.iPhone.createNavigationGroup({
-  window:first
+  window:win
 });
 
 
@@ -23,5 +24,4 @@ var main = Ti.UI.createWindow();
 main.add(navGroup);
 main.open({
 	transition:Ti.UI.iPhone.AnimationStyle.CURL_UP
-
 });
